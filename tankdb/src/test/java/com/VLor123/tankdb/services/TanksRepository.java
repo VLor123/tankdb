074e24b06508aa374566b64000f3fabdbc941a05
VLor123 <90496392+VLor123@users.noreply.github.com>
2024-12-21 19:32:50 +0100

    Delete tankdb/src/test/java/com/VLor123/tankdb/services directory

--- tankdb/src/test/java/com/VLor123/tankdb/services/TanksRepository.java
+++
@@ -1,7 +0,0 @@
-package com.VLor123.tankdb.services;
-
-import com.VLor123.tankdb.models.Tank;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TanksRepository extends JpaRepository<Tank, Integer> {
-}
