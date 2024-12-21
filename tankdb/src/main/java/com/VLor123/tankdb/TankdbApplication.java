e7f40cca6ca09a12b01eee08e9328c94d2c7646d
VLor123 <90496392+VLor123@users.noreply.github.com>
2024-12-21 19:05:05 +0100

    Add files via upload

---
+++ tankdb/src/main/java/com/VLor123/tankdb/TankdbApplication.java
@@ -0,0 +1,13 @@
+package com.VLor123.tankdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TankdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TankdbApplication.class, args);
+	}
+
+}
---
+++ tankdb/src/test/java/com/VLor123/tankdb/TankdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VLor123.tankdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TankdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
